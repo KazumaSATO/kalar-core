275cf0ca7e56fe3711089eedf39469cd6feec6ee
Kazuma SATO <ranceworks@gmail.com>
2015-11-28 09:56:10 +0900

    #2 load plugin mechanism

--- src/kalar_core/server.clj
+++ src/kalar_core/server.clj
@@ -1,12 +1,24 @@
 (ns kalar-core.server
-  (:require [kalar-core.config :refer [read-config]]))
+  (:require [kalar-core.config :refer [read-config]]
+             [compojure.core :refer [GET defroutes]]
+             [compojure.route :as route]
+             [ring.util.response :refer [redirect]]
+             ))
+
+(defn load-plugins [plugins]
+  (dorun (for [plugin plugins]
+           (let [nmspc (-> (re-seq #"^[^/]*" (str plugin)) first symbol)]
+             (require nmspc)
+             ((-> plugin resolve))))))
 
 (defn init []
   (let [config (read-config)
-        plugins (-> config :plugins)
-        lst (nth plugins 0)
-        f (nth lst 0)
-        a (nth lst 1)]
-    (println f)
-    (println a)
-    (-> 'kalar-core.server/init resolve println) ))
+        plugins (-> config :plugins)]
+    (load-plugins plugins)))
+
+
+
+(defroutes handler
+  (GET ":prefix{.*}/" [prefix] (redirect (str prefix "/index.html")))
+  (route/resources "/" {:root "_site"})
+  (route/not-found "Page not found"))
