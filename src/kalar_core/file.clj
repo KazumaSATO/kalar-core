6b8adaa8844a68da15d3f22897697939cae2505f
Kazuma SATO <ranceworks@gmail.com>
2015-11-30 20:22:49 +0900

    #6 resources

--- src/kalar_core/file.clj
+++ src/kalar_core/file.clj
@@ -2,10 +2,16 @@
   (:require [clojure.java.io :as io]
             [kalar-core.config :as config]))
 
+(defn find-resources-dir []
+  (.getAbsolutePath (io/file "resources")))
+
 (defn find-dest []
-  (let [f (io/file "resources" (:dest (config/read-config)))]
+  "returns a build destination as string."
+  (let [f (io/file (find-resources-dir) (:dest (config/read-config)))]
     (.getAbsolutePath f)))
 
 (defn prepare-write-file [maybe-unexist-file]
   (.mkdirs (.getParentFile maybe-unexist-file))
   (.createNewFile maybe-unexist-file))
+
+
