8eaf60889104977ad0ff0c62c76e495208e25ab3
Kazuma SATO <ranceworks@gmail.com>
2015-12-09 06:23:03 +0900

    #10 add func

--- src/kalar_core/file.clj
+++ src/kalar_core/file.clj
@@ -1,9 +1,9 @@
 (ns kalar-core.file
   (:require [clojure.java.io :as io]
+            [clojure.string :as str]
             [kalar-core.config :as config]))
 
 (defn find-resources-dir []
-  "DEPRECATED"
   (.getAbsolutePath (io/file "resources")))
 
 (defn find-dest []
@@ -12,7 +12,10 @@
     (.getAbsolutePath f)))
 
 (defn touch [maybe-unexist-file]
+  "Refactoring: rename to create-empty-file"
   (.mkdirs (.getParentFile maybe-unexist-file))
   (.createNewFile maybe-unexist-file))
 
 
+(defn get-dst [file-path]
+  (io/file (find-dest) (str/replace file-path (re-pattern (str "^" (find-resources-dir) "/")) "")))
