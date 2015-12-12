e3b1d1d9444fd2b6f6c40238adca4607a9679240
Kazuma SATO <ranceworks@gmail.com>
2015-12-12 13:41:33 +0900

    Change the license to the MIT license #11

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject kalar-core "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/KazumaSATO/kalar-core"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
                  [me.raynes/fs "1.4.6"]])
